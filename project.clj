07639b3440aa01a758c768ace32717013652fb56
Joseph Guhlin <joseph.guhlin@gmail.com>
2018-10-24 10:20:46 +1300

    Updated dependencies, begun removing pulsar/quasar libraries

--- project.clj
+++ project.clj
@@ -3,28 +3,29 @@
   :aot [odg.core]
   :profiles {
                               :uberjar { :aot :all}}
-  
+
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :jvm-opts ^:replace [
                        "-Xms6G"
-                       "-Xmx6G"
+                       "-Xmx6G"]
                        ;"-XX:MaxDirectMemorySize=1G" ; May have caused a huge slowdown? At least in eclipse...
                        ;"-XX:+UseParallelGC"
-                       "-XX:+UseConcMarkSweepGC"
-                       "-XX:+UseCondCardMark"
-                       "-XX:+UseBiasedLocking"
-                       "-XX:+AggressiveOpts"
-                       "-XX:+UseCompressedOops"
-                       "-XX:+UseFastAccessorMethods"
-                       "-XX:+DoEscapeAnalysis"
-                       "-Xss64M"
-                       "-d64"
-                       "-server"
-                       "-Dco.paralleluniverse.fibers.detectRunawayFibers=false"
+                       ;"-XX:+UseConcMarkSweepGC"
+                       ;"-XX:+UseCondCardMark"
+                       ;"-XX:+UseBiasedLocking"
+                       ;"-XX:+AggressiveOpts"
+                       ;"-XX:+UseCompressedOops"
+                       ;"-XX:+UseFastAccessorMethods"
+                       ;"-XX:+DoEscapeAnalysis"
+                       ;"-Xss64M"
+                       ;"-d64"
+                       ;"-server"
+                       ;"-Dco.paralleluniverse.fibers.detectRunawayFibers=false"
                        ;"-Dco.paralleluniverse.fibers.verifyInstrumentation"
-                       "-XX:-OmitStackTraceInFastThrow"]
-  
+                       ;"-XX:-OmitStackTraceInFastThrow"
+
+
   :plugins [[codox "0.6.6"]
             [lein-ring "0.8.11"]]
   :ring {:handler odg.query-server/handler
@@ -32,16 +33,12 @@
          :init odg.query-server/dev-init
          :auto-reload? true
          :auto-refresh? true}
-  
-  :java-agents [[co.paralleluniverse/quasar-core "0.7.8"]]
+
   :resource-paths ["resources"]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [co.paralleluniverse/pulsar "0.7.8"]
-                 [co.paralleluniverse/quasar-core "0.7.8"]
-                 [co.paralleluniverse/quasar-actors "0.7.8"]
-                 [org.neo4j/neo4j "3.2.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.neo4j/neo4j "3.4.9"]
                  [clojure-csv/clojure-csv "2.0.2"]
-                 [cheshire "5.7.1"]
+                 [cheshire "5.8.1"]
                  [org.clojure/tools.cli "0.3.1"]
                  [criterium "0.4.4"]
                  [org.clojure/math.combinatorics "0.1.4"]
@@ -50,18 +47,14 @@
                  [foldable-seq "0.2"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/data.zip "0.1.2"]
-                 [incanter "1.5.5"]
-                 [digest "1.4.5"]
+                 [incanter "1.9.3"]
+                 [digest "1.4.8"]
                  [biotools "0.1.1-b1"]
                  [com.taoensso/timbre "4.10.0"]
                  [me.raynes/fs "1.4.6"]
-                 [ring/ring-core "1.6.1"] ; :exclusions [org.clojure/tools.reader]]
-                 [ring/ring-jetty-adapter "1.6.1"]
+                 [ring/ring-core "1.7.0"] ; :exclusions [org.clojure/tools.reader]]
+                 [ring/ring-jetty-adapter "1.7.0"]
                  [ring/ring-json "0.4.0"]
-                 [liberator "0.14.1"]
-                 [compojure "1.6.0"] 
-                 [org.clojure/core.memoize "0.5.9"]])
-                 
-
-
-
+                 [liberator "0.15.2"]
+                 [compojure "1.6.1"]
+                 [org.clojure/core.memoize "0.7.1"]])
