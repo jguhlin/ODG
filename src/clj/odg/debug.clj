ba3f4ff1b6371c3e2d81c7a8deed6c9b8059e0ba
Joseph Guhlin <joseph.guhlin@gmail.com>
2018-11-06 15:26:46 +1300

    .

--- src/clj/odg/debug.clj
+++ src/clj/odg/debug.clj
@@ -1,3 +1,4 @@
+(dbh/connect "repl.db" "4G")
 (def assembly (odg.assembly/import-fasta "Vespula vulgaris" "1" "data/vvul/processed_assembly.masked.fasta"))
 (def annotation (odg.annotation/import-gff "Vespula vulgaris" "1" "data/vvul/augustus.hints.gff3"))
 (def proteome (odg.proteome/import-fasta "Vespula vulgaris" "1" "data/vvul/augustus.hints.aa"))
